c89944594d221849a7483dfef285875b80e12348
CanUK <craig.stanton2@gmail.com>
2019-01-27 21:21:24 +0000

    multi pages and links

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# General variables to be used across all pages
+site_components = ['Home', 'Blog', 'About']
+
 @app.route('/')
 def home():
     text = 'The home page will contain initiatives, links to the blog, coding progress, etc.'
-    site_components = ['Home', 'Blog', 'About']
     return render_template('home.html', text = text, site_components = site_components)
 
 @app.route('/blog/')
 def blog():
-    return 'Blog posts'
+    text = "The place where I will record my development progress and write down my thoughts"
+    return render_template('blog.html', text = text, site_components = site_components)
 
 @app.route('/about/')
 def about():
-    return 'Content about me and what my goals are'
+    text = 'Content about me and what my goals are'
+    return render_template('about.html', text = text, site_components = site_components)
 
 if __name__ == '__main__':
     app.debug = True
