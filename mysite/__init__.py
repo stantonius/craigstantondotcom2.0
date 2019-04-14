0e76e2ba2343672cf6c6a676c27f9dacd569d51a
CanUK <craig.stanton2@gmail.com>
2019-04-14 19:53:01 +0100

    edit changes

--- mysite/__init__.py
+++ mysite/__init__.py
@@ -6,8 +6,12 @@ from flask_login import LoginManager
 from flask_admin import Admin
 import socket
 from flask_ckeditor import CKEditor
+import warnings
+import datetime
 
 from oauth2client.contrib.flask_util import UserOAuth2
+
+warnings.filterwarnings("ignore", "Your application has authenticated using end user credentials")
 
 
 
@@ -26,3 +30,12 @@ oauth2 = UserOAuth2()
 oauth2.init_app(
         app,
         scopes=['email', 'profile'])
+
+@app.template_filter('strftime')
+def _jinja2_filter_datetime(date, fmt=None):
+    format=r"%d %B %Y"
+    date = datetime.datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    if fmt:
+        return date.strftime(fmt)
+    else:
+        return date.strftime(format)
